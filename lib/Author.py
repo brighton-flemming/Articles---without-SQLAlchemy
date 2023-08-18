bce669b613d8c5c9221be2e9428a5c675642fb1d
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-18 19:39:52 +0300

    Mission Passed

--- lib/Author.py
+++ lib/Author.py
@@ -1,7 +1,13 @@
 class Author :
     def __init__(self, id, name) :
         self.id = id
-        self.name = name
+        self._name = name
         self.articles = []
     def add_article(self, article):
         self.articles.append(article)
+    def name(self):
+        return self._name
+
+author = Author("John Doe")
+
+print("Author's name:", author.name())

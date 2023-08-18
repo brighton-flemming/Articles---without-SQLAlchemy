3a3f7799cfbeb452b3a096e7c4eec5e1dcbf885d
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-19 00:52:12 +0300

    Mission Passed

--- lib/Magazine.py
+++ lib/Magazine.py
@@ -1,9 +1,11 @@
+from Author import author1, author2
 class Magazine:
     _magazines = []
     def __init__(self, id, name, category):
        self.id = id
        self._name = name
        self._category = category
+       self._contributors = []
        Magazine._magazines.append(self)
  
     
@@ -13,6 +15,12 @@ class Magazine:
     def category(self):
         return self._category
     
+    def add_contributor(self, author):
+        self._contributors.append(author)
+
+    def contributor(self):
+        return self._contributors
+    
     def all():
         return Magazine._magazines
 
@@ -21,6 +29,12 @@ magazine2 = Magazine(2, "Science Journal", "Science")
 magazine3 = Magazine(3, "Fashion Weekly", "Fashion")
 magazine4 = Magazine(4, "Motor Trend", "Automobile")
 
+magazine1.add_contributor(author1)
+magazine2.add_contributor(author2)
+magazine3.add_contributor(author1)
+magazine4.add_contributor(author2)
+
+
 
 print("Magazine 2's name:", magazine2.name())
 print("Magazine 2's category:", magazine2.category())
@@ -28,3 +42,7 @@ print("Magazine 2's category:", magazine2.category())
 all_magazines = Magazine.all()
 for magazine in all_magazines:
     print("Magazine:", magazine.name(), "-Category:", magazine.category())
+
+print("Contributors for Magazine 2: ")
+for contributor in magazine2.contributors():
+    print("-", contributor.name())

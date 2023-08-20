772b3ca7ae68c9df31a54669e7ca610f79b9a4f7
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-20 14:49:54 +0300

    Mission Passed

--- lib/Author.py
+++ lib/Author.py
@@ -36,27 +36,6 @@ author3 = Author(2, "Patrick Benjamin")
 author4 = Author(2, "Patrick Benjamin")
 
 
-
-# article1 = Article("author1", magazine1, "Introduction to ORM")
-# article2 = Article("author1", magazine2, "Advances in AI")
-# article3 = Article("author2", magazine3, "Exploring Quantum Computing")
-# article4 = Article("author2", magazine4, "$300K 2025 Ford Mustang GTD Puts Europe's Best on Notice")
-
-# author1.add_article(article1)
-# author1.add_article(article4)
-# author2.add_article(article2)
-# author2.add_article(article3)
-
-# print("Author 1's articles:")
-# for article in author1.articles():
-#     print("-", article.title())
-
-# print("Author 1's articles:")
-# for magazine in author1.magazines():
-#     print("-", magazine.name())
-
-
-
 print("Author's name:", author1.name())
 print("Author's name:", author2.name())
 print("Author's name:", author3.name())
